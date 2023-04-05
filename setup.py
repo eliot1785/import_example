fb590902cd7a193220e4e1b3c774f2684b90d28f
stephenonethree <stephen@onethree.bio>
2023-04-05 18:28:36 -0400

    Add more things to setup

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='myfol1',
-    version='0.0.4',
+    version='0.0.5',
+    author='xyz',
+    author_email='xyz@shitmail.com',
+    description='Testing installation of Package',
+    long_description='testing installation of package',
+    url='https://github.com/Muls/toolbox',
     packages=['myfol1']
 )
