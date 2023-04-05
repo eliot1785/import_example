d6f77229061439e602d678294ebe32cf09e1d6ed
stephenonethree <stephen@onethree.bio>
2023-04-05 18:44:20 -0400

    Fix packages listing, remove extraneous __init__.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='myfol1',
-    version='0.0.5',
+    version='0.0.6',
     author='xyz',
     author_email='xyz@shitmail.com',
     description='Testing installation of Package',
     long_description='testing installation of package',
     url='https://github.com/Muls/toolbox',
-    packages=['myfol1']
+    packages=['myfol1','myfol1.myfol2','myfol1.myfol2.myfol3']
 )
